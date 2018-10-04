a2b334a6f85158a0bd46adca5da6884c262a8359
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-10-04 21:02:13 +0200

    basic ls and ln in cmd

---
+++ BashToBatch/build.bat
@@ -0,0 +1,5 @@
+pyinstaller batch_ls.py -F -n ls -i icon.ico
+pyinstaller batch_ll.py -F -n ll -i icon.ico
+
+REM bash -c 'pyinstaller batch_ls.py -F -n ls'
+REM bash -c 'pyinstaller batch_ll.py -F -n ll'
