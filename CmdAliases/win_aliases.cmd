f662d9fdb3b90d69332bd3226b1f29de2325103a
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-01-14 21:40:23 +0100

    Added vim nvim reboot

--- CmdAliases/win_aliases.cmd
+++ CmdAliases/win_aliases.cmd
@@ -6,6 +6,8 @@ DOSKEY ll=dir /W
 
 DOSKEY ifconfig=ipconfig /all
 
+DOSKEY reboot=shutdown -r
+
 :: program > bash
 DOSKEY vim=bash.exe -c 'vim $1 $2 $3 $4 $5 $6 $7 $8 $9'
 
@@ -13,3 +15,5 @@ DOSKEY vim=bash.exe -c 'vim $1 $2 $3 $4 $5 $6 $7 $8 $9'
 DOSKEY apt=bash.exe -c 'apt $1 $2 $3 $4 $5 $6 $7 $8 $9'
 DOSKEY apt-get=bash.exe -c 'apt-get $1 $2 $3 $4 $5 $6 $7 $8 $9'
 DOSKEY sudo=bash.exe -c 'sudo $1 $2 $3 $4 $5 $6 $7 $8 $9'
+DOSKEY vim=bash.exe -c 'vim $1 $2 $3 $4 $5 $6 $7 $8 $9'
+DOSKEY nvim=bash.exe -c 'nvim $1 $2 $3 $4 $5 $6 $7 $8 $9'
