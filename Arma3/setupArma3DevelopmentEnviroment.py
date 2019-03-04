b34d4d8274f6573cedc57bbfe7dc73d33d2edc6c
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-03-04 23:12:55 +0100

    Updated Tool

--- Arma3/setupArma3DevelopmentEnviroment.py
+++ Arma3/setupArma3DevelopmentEnviroment.py
@@ -3,86 +3,94 @@ import os, sys
 import winreg
 
 
-def setupPDrive(TOOL_LOCATION,LOCATION_TO_P=''):
+def setup_PDrive(tool_path,pdrive=''):
     if not os.path.exists('P:'):
-        os.system('start /d "{}" WorkDrive.exe /Mount {}'.format(TOOL_LOCATION,LOCATION_TO_P))
+        os.system('start /d "{}" WorkDrive.exe /Mount {}'.format(tool_path,pdrive))
+        print('Setting up workdrive path.'.format())
     else:
-        os.chdir("P:")
+        print('P drive already exists skipping the creation.'.format())
+        
+
+def create_mount_unmount(tool_path,pdrive=''):
+    print('Creating quick mount and unmount shortcuts.')
+    os.system('echo start /d "{}" WorkDrive.exe /Mount {} > PDrive_mount.cmd'.format(tool_path,pdrive))
+    os.system('echo start /d "{}" WorkDrive.exe /Dismount > PDrive_umount.cmd'.format(tool_path,pdrive))
+
+
+def request_action(text='Continue?'):
+    Continue_Count = 0
+    while(True):
+        yes_no = input('{} (Yes or No)\n> '.format(text))
+        yes_no = yes_no.lower()
+        if (yes_no == 'yes' or yes_no == 'y'):
+            return True
+        elif (yes_no == 'no' or yes_no == 'n'):
+            return False
+        else:
+            pass
+        Continue_Count += 1
+        if Continue_Count >= 3:
+            sys.exit()
+
+
+def get_program_path_HKCU(lable='Program', regKey='', key='path'):
+    print('Checking for {}...'.format(lable))
+    try:
+        registry = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        keypath = winreg.OpenKey(registry, regKey)
+    except:
+        sys.exit('{} is not installed.'.format(lable))
+    try:
+        path = winreg.QueryValueEx(keypath, key)
+        print('{} found...'.format(lable))
+        return path[0]
+    except:
+        sys.exit('Some thing whent wrong when looking for {}'.format(key))
+
+
+def get_program_path_HKLM(lable='Program', regKey='', key='main'):
+    print('Checking for {}.'.format(lable))
+    try:
+        registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        keypath = winreg.OpenKey(registry, regKey)
+    except:
+        sys.exit('{} is not installed.'.format(lable))
+    try:
+        path = winreg.QueryValueEx(keypath, key)
+        print('{} found...'.format(lable))
+        return path[0]
+    except:
+        sys.exit('Some thing whent wrong when looking for {}'.format(key))
 
 
 def main():
     # Get script path
     scriptPath = os.path.realpath(__file__)
     projectPath = os.path.dirname(scriptPath)
-    
-    # Get ARMA TOOLS; workdrive
-    try:
-        workdrive_reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        workdrive_key = winreg.OpenKey(workdrive_reg, 'Software\\Bohemia Interactive\\workdrive')
-    except:
-        sys.exit('Arma 3 Tool is not installed.')
-    try:
-        workdrive_val = winreg.QueryValueEx(workdrive_key, 'path')
-    except:
-        sys.exit('Arma 3 Tool is not installed.')
-    workdrive_path = workdrive_val[0]
 
-    # Check and set up P_DRIVE if it does not exist
-    setupPDrive(workdrive_path,projectPath)
+    linking = get_program_path_HKCU('Arma 3 Tool - Workdrive','Software\\Bohemia Interactive\\workdrive')
+    check = request_action('Is "{}" were you whant to set up your arma 3 P drive?'.format(projectPath))
+    if check:
+        setup_PDrive(linking,projectPath)
+        create_mount_unmount(linking,projectPath)
+    else:
+        custom_path = input('Type out full path: ')
+        setup_PDrive(linking,custom_path)
+        create_mount_unmount(linking,custom_path)
 
     os.chdir(projectPath)
 
-    # Get Arma3 ROOT
-    try:
-        arma_reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-        arma_key = winreg.OpenKey(arma_reg, 'Software\\wow6432Node\\bohemia interactive\\arma 3')
-    except:
-        sys.exit('Arma 3 is not installed.')
-    try:
-        arma_val = winreg.QueryValueEx(arma_key, 'main')
-    except:
-        sys.exit('Arma 3 is not installed.')
-    arma_path = arma_val[0]
-    os.system('mklink /J "! ROOT" "{}"'.format(arma_path))
+    linking = get_program_path_HKLM('Arma 3','Software\\wow6432Node\\bohemia interactive\\arma 3')
+    os.system('mklink /J "! ROOT" "{}"'.format(linking))
+    if not os.path.exists('{}/!Workshop'.format(linking)):
+        os.system('mklink /J "! Workshop" "{}/!Workshop"'.format(linking))
 
+    linking = get_program_path_HKCU('Arma 3 Tool','Software\\Bohemia Interactive\\arma 3 tools')
+    os.system('mklink /J "! Arma 3 Tools" "{}"'.format(linking))
 
-    # Check if !Workshop exist in Arma Root
-    if not os.path.exists('{}\\!Workshop'.format(arma_path)):
-        os.system('mklink /J "Workshop" "{}\\!Workshop"'.format(arma_path))
-    else:
-        pass
-
-
-
-    # Get TOOLS
-    try:
-        tool_reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        tool_key = winreg.OpenKey(tool_reg, 'Software\\Bohemia Interactive\\arma 3 tools')
-    except:
-        sys.exit('Arma 3 Tool is not installed.')
-    try:
-        tool_val = winreg.QueryValueEx(tool_key, 'path')
-    except:
-        sys.exit('Arma 3 Tool is not installed.')
-    tool_path = tool_val[0]
-    os.system('mklink /J "! Tools" "{}"'.format(tool_path))
-
-
-
-    # Get TOOLS Mikero
-    try:
-        mikero_reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        mikero_key = winreg.OpenKey(mikero_reg, 'Software\Mikero\pboProject')
-    except:
-        print('Mikero Tool is not installed. Download it from here: https://mikero.bytex.digital/Downloads')
-    try:
-        mikero_val = winreg.QueryValueEx(mikero_key, 'path')
-        mikero_path = mikero_val[0]
-        os.system('mklink /J "! Mikero Tools" "{}"'.format(mikero_path))
-    except:
-        pass
-
-
+    linking = get_program_path_HKCU('Mikero Tool','Software\Mikero\pboProject')
+    os.system('mklink /J "! Workshop" "{}\\!Workshop"'.format(linking))
+    os.system('mklink "Unpack_A3" "{}/bin/Arma3P.cmd"'.format(linking))
 
     # Get AppData
     appdata_path = '%userprofile%\\appdata\\local\\Arma 3\\'
@@ -90,27 +98,6 @@ def main():
         os.system('mklink /J "! AppData" "{}"'.format(appdata_path))
     else:
         pass
-
-
-
-    # get Arma 3 profiles
-    # profile = "Dokument\\Arma"
-    # other_profile = "Dokument\\Arma 3 - Other Profiles"
-
-    # if os.path.exists(profile):
-    #     print('exist!')
-    # else:
-    #     print('dont exist!')
-
-    # if os.path.exists(other_profile):
-    #     print('exist!')
-    # else:
-    #     print('dont exist!')
-
-    # other_profiles = os.listdir(other_profile)
-    # print(other_profiles)
-
-    # select valid
 
 
 if __name__ == "__main__":
