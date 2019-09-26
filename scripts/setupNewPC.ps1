f759fb9b87e25f999ae3f8d9b9326ff88476db40
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-09-26 22:43:48 +0200

    Adjusted and updated script

--- scripts/setupNewPC.ps1
+++ scripts/setupNewPC.ps1
@@ -4,19 +4,19 @@ iwr -useb get.scoop.sh | iex
 
 echo Installing scoop packages...
 scoop install sudo
-sudo scoop install aria2 git 7zip go
+scoop install aria2 git 7zip go
 scoop install sudo --global
-sudo scoop install aria2 git 7zip go --global
+scoop install aria2 git 7zip go --global
 
-sudo scoop install curl grep sed less touch ruby perl openssh --global
-sudo scoop install ripgrep neovim adb  --global
-sudo scoop install python --global
+scoop install curl grep sed less touch ruby perl openssh --global
+scoop install ripgrep neovim adb  --global
+scoop install python --global
 
 scoop bucket add extras
-sudo scoop install steamcmd qbittorrent-portable --global
+scoop install steamcmd qbittorrent-portable --global
 
 scoop bucket add Arma3Tools https://github.com/ColdEvul/arma3-scoop-bucket.git
-sudo scoop install armake hemtt --global
+scoop install armake hemtt --global
 
 
 echo Installing Chocolately...
@@ -26,10 +26,19 @@ echo Changeing chocolately settings...
 choco feature enable -n allowGlobalConfirmation
 
 echo Installing chocolately packages...
-sudo choco install googlechrome
-sudo choco install vscode
-sudo choco install microsoft-windows-terminal
-sudo choco install steam
-sudo choco install winrar
-sudo choco install vlc
-sudo choco install TortoiseGit
+choco install googlechrome
+choco install vscode
+choco install microsoft-windows-terminal
+choco install steam
+choco install winrar
+choco install vlc
+choco install TortoiseGit
+choco install teamspeak
+choco install discord
+choco install slack
+choco install spotify
+choco install teamviewer
+
+echo Downloading drives and programs for gaming...
+Invoke-WebRequest https://s3.amazonaws.com/naturalpoint/trackir/software/TrackIR_5.4.2.exe -OutFile "$Env:userprofile/Downloads/TrackIR_5.4.2.exe"
+Invoke-WebRequest https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_9.00.42_x86_Logitech.exe -OutFile "$Env:userprofile/Downloads/LGS_9.00.42_x86_Logitech.exe"
