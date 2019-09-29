e574f2d024618ad1a0c90ca66b718433a86007a8
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-09-29 16:17:37 +0200

    Major cleanup

--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 Here you can find scripts and regestry edits to custumize your computer more.
+
+- **Arma3Development** - Scripts that related to Arma Development
+- **CMDPowershellParallel** - This registry edit allow you to open a cmd terminal as well as powershell in the context menu.
+- **CMDShellColorThemes** - Registry way edit and changes the font for the terminals in windows CMD and WSL defult terminals.
+- **CustomNewFileRegFiles** - Allow you to create more file types in the *New*  the context menu.
+- **Library** - Saved configs
+- **Scripts** - Contain misc scripts, for new installed systems.
+- **Tools** - Tools for difrent sort of tasks.
+- **VirtualTerminal** - Allow colors in windows terminals. If it does not work out of the box.
+- **VS Code** - VS Code regestry edits
+- **WindowsBatchRC** - A .bashrc copy but for windows.
+- **WindowsDefender** - Disabler and removal of defender from context menu.
+- **WindowsTerminalThemes** - Saved converted JSON style files for [Windows Terminal](https://github.com/microsoft/terminal). More can be found [here](https://github.com/mbadolato/iTerm2-Color-Schemes).
---
+++ scripts/setupNewLinux.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+IS_WSL=/mnt/c/WINDOWS/system32/wsl.exe
+if [ -x "$IS_WSL" ]; then
+    python3 setupNewWSL.py
+fi
+
+######################################################
+sudo apt update -y
+sudo apt full-upgrade -y
+
+sudo apt install git -y
+sudo apt install neovim opessh android-sdk dos2unix ripgrep -y
+sudo apt install ninja rust python3 -y
+
+sudo apt install steam-installer -y
+
+######################################################
+if [ ! -x "$IS_WSL" ]; then
+    sudo apt install snapd -y
+
+    sudo snap install code --classic
+
+    sudo snap install spotify
+
+    sudo snap install discord
+    sudo snap install slack --classic
+fi
+
+######################################################
+echo Adding check for PATH in .bashrc...
+echo """
+
+# PATH definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_path, instead of adding them here directly.
+if [ -f ~/.bash_path ]; then
+    . ~/.bash_path
+fi
+""" >> ~/.bashrc
+
+
+######################################################
+echo Setting up .scripts... 
+mkdir ~/.scripts
+echo """#!/usr/bin/env bash 
+
+set -e >> ~/.scripts/adb-push
+
+adb push $1 /sdcard/Download/$2
+echo $1: have been pushed to ./sdcard/Download/$2
+
+chmod +x ~/.scripts
+""" >> ~/.scripts/adb-push
+
+echo Setting up path... 
+echo """
+export PATH=\$PATH:~/.scripts
+
+# Android
+export ANDROID_HOME=$HOME/android-sdk
+export PATH=\$PATH:$ANDROID_HOME/tools/bin
+export PATH=\$PATH:$ANDROID_HOME/platform-tools
+""" >> ~/.bash_path
+
+
+######################################################
+echo Setting up .bash_aliases... 
+echo """ 
+alias dir='ls'
+
+alias sys-update='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y; sudo snap refresh; echo All updates are completed.'
+
+# Python
+alias pip='pip3'
+alias py='python3'
+alias python='python3'
+
+# Sudo
+alias inperium='sudo'
+
+# Program
+alias vim='nvim'
+
+alias ncspot='~/Library/ncspot/target/release/ncspot'
+alias spotify='ncspot'
+alias spotify-doc='google-chrome --app=https://github.com/hrkfdn/ncspot/blob/develop/README.md'
+
+alias chrome='google-chrome'
+alias screen='echo Running: scrcpy; scrcpy'
+alias youtube='chrome --app=https://youtube.com/'
+alias ryver='chrome --app=https://playipp.ryver.com/index.html#users/1494170/chat'
+alias sneek='chrome --app=https://sneek.io/app/playipp'
+
+# ADB shorts
+alias adb-reverse='adb reverse tcp:8081 tcp:8081'
+alias adb-r='adb-reverse'
+alias adb-p='adb-push'
+
+alias rn-android-uninstall='adb-u; rn-a'
+alias rn-au='rn-android-uninstall'
+
+# Git shortcuts
+alias g-r='git rebase'
+alias g-f='git fetch --all --prune'
+
+alias g-fr='git fetch --all --prune; git rebase'
+alias g-rf='g-fr'
+
+alias g-s='git status'
+alias g-c='git checkout'
+alias g-u='git rebase master'
+
+alias g-p='git stash'
+alias g-pp='git stash pop'
+
+alias g-frp='g-p; g-rf'
+alias g-rfp='g-frp'
+alias g-frpp='g-frp; g-pp' 
+alias g-rfpp='g-rfpp'
+""" >> ~/.bash_aliases
