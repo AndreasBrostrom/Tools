da05583578cbc4f3a18ff4a7d1320a0f71488ed7
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-10-12 11:51:00 +0200

    Improved the handeling of installations

--- scripts/setupNewWin.ps1
+++ scripts/setupNewWin.ps1
@@ -1,82 +1,101 @@
 #Requires -RunAsAdministrator
+# Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
-Write-Host "Starting up..." -ForegroundColor green
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+# GLOBALS
+$scoop_buckets    = 'extras', 'Arma3Tools https://github.com/ColdEvul/arma3-scoop-bucket.git'
+
+$scoop_pkg        = 'sudo', 'aria2', 'git', '7zip', 'curl',
+                    'grep', 'ripgrep', 'sed', 'touch', 'jq', 'dos2unix',
+                    'openssh', 'neovim', 'gdrive', 'scrcpy',
+                    'python', 'ruby', 'msys2', 'perl', 'ninja', 'rust',
+                    'steamcmd', 'qbittorrent-portable', 'android-sdk', 'rufus',
+                    'armake', 'hemtt'
+
+$choco_pkg        = 'googlechrome', 'vscode',
+                    'microsoft-windows-terminal',
+                    'winrar', 'vlc', 'spotify', 'teamviewer', 'TortoiseGit',
+                    'teamspeak', 'discord', 'slack',
+                    'steam',
+                    'obs-studio',
+                    'powershell-core'
+
+# Script start
+Write-Host "Starting up..." -ForegroundColor Blue
+
+
 
 # Installing scoop
+Write-Host "Setting up Scoop..." -ForegroundColor Blue
 if (![System.IO.File]::Exists("$env:USERPROFILE\scoop\shims\scoop")) {
-    Write-Host "Installing Scoop..." -ForegroundColor green
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-} else { Write-Host "Scoop already exist..." -ForegroundColor Yellow }
+    Write-Host "Installing Scoop..." -ForegroundColor Blue
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') >$null 2>&1
+} else {
+    Write-Host "Scoop already exist no need to install. Checking for updates..." -ForegroundColor Yellow
+    scoop update scoop >$null 2>&1
+}
 
-Write-Output "Installing Scoop packages..."
-scoop install aria2 git 7zip
-scoop install sudo
-scoop install sudo --global
-scoop install aria2 git 7zip --global
+# Add scoop buckets
+Write-Host "Adding Scoop buckets..."
+foreach ($buckets in $scoop_buckets) {
+    scoop bucket add $buckets >$null 2>&1
+}
 
-scoop install curl grep ripgrep sed touch openssh --global
-scoop install neovim gdrive jq dos2unix scrcpy --global
-scoop install python ruby msys2 perl ninja rust --global
+# Install scoop packages
+Write-Host "Installing Scoop packages..."
+foreach ($pkg in $scoop_pkg) {
+    if (![System.IO.Directory]::Exists("$env:PROGRAMDATA\scoop\apps\$pkg")) {
+        Write-Host "Installing $pkg..."
+        scoop install $pkg --global >$null 2>&1
+    } else {
+        Write-Host "Scoop $pkg already installed skipping..." -ForegroundColor Yellow
+    }
+}
+Write-Host "Installation of scoop packages completed..." -ForegroundColor Green
 
-scoop bucket add extras
-scoop install steamcmd qbittorrent-portable android-sdk rufus --global
-
-scoop bucket add Arma3Tools https://github.com/ColdEvul/arma3-scoop-bucket.git
-scoop install armake hemtt --global
-
-Write-Host "Installation of scoop packages completed..." -ForegroundColor blue
 
 
-# Installing chocolately
+# Installing Chocolately
+Write-Host "Setting up Chocolately..." -ForegroundColor Blue
 if (![System.IO.File]::Exists("C:\ProgramData\chocolatey\choco.exe")) {
     Write-Host "Installing Chocolately..." -ForegroundColor green
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 } else { Write-Host "Chocolately already exist..." -ForegroundColor Yellow }
 
-Write-Output "Changeing and setting some paths for chocolately..."
-choco feature enable -n allowGlobalConfirmation
+Write-Host "Changeing and setting some paths for Chocolately..."
+choco feature enable -n allowGlobalConfirmation >$null 2>&1
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\ProgramData\Chocolatey\tools", "Machine")
 
-Write-Output "Installing chocolately packages..."
-choco install googlechrome
-choco install vscode
+# Chocolately packages
+Write-Host "Installing Chocolately packages..."
+foreach ($pkg in $choco_pkg) {
+    choco search steam --local >$null 2>&1
+    if (!$?) {
+        Write-Host "Installing $pkg..."
+        choco install $pkg >$null 2>&1
+    } else {
+        Write-Host "Choco $pkg already installed skipping..." -ForegroundColor Yellow
+    }
+}
+Write-Host "Installation of chocolately packages completed..." -ForegroundColor Green
 
-choco install microsoft-windows-terminal
-
-choco install winrar
-choco install vlc
-choco install spotify
-choco install teamviewer
-choco install TortoiseGit
-
-choco install teamspeak
-choco install discord
-choco install slack
-
-choco install steam
-choco install obs-studio
-
-choco install powershell-core
-
-Write-Host "Installation of chocolately packages completed..." -ForegroundColor blue
 
 
 # Download drives and packages for gaming
-Write-Host "Downloading drives and programs for gaming..." -ForegroundColor green
-Invoke-WebRequest https://s3.amazonaws.com/naturalpoint/trackir/software/TrackIR_5.4.2.exe -OutFile "$Env:userprofile\Downloads\TrackIR_5.4.2.exe"
+Write-Host "Downloading drives and programs for gaming..." -ForegroundColor Blue
+Invoke-WebRequest https://s3.amazonaws.com/naturalpoint/trackir/software/TrackIR_5.4.2.exe -OutFile "$Env:userprofile\Downloads\TrackIR_5.4.2.exe" >$null 2>&1
 
-Invoke-WebRequest https://media.roccat.org/driver/Tyon/ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip -OutFile "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip"
-Expand-Archive "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip" -DestinationPath "$Env:userprofile\Downloads\"
-Remove-Item "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip"
+Invoke-WebRequest https://media.roccat.org/driver/Tyon/ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip -OutFile "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip" >$null 2>&1
+Expand-Archive "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip" -DestinationPath "$Env:userprofile\Downloads\" >$null 2>&1
+Remove-Item "$Env:userprofile\Downloads\ROCCAT_Tyon_DRV1.17_FW1.34forAlienFx-v1.zip" >$null 2>&1
 
-Invoke-WebRequest https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_9.02.65_x64_Logitech.exe -OutFile "$Env:userprofile/Downloads/LGS_9.02.65_x64_Logitech.exe"
+Invoke-WebRequest https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_9.02.65_x64_Logitech.exe -OutFile "$Env:userprofile/Downloads/LGS_9.02.65_x64_Logitech.exe" >$null 2>&1
 
-Write-Host "Drives packages downloaded and ready..." -ForegroundColor blue
+Write-Host "Drives packages downloaded and ready..." -ForegroundColor Green
+
 
 
 # Setting up home enviroment
-Write-Output "Setting up home..."
+Write-Host "Setting up home..."
 
 New-Item -itemtype "directory" -path "$Env:userprofile\.scripts"
 (get-item $Env:userprofile\.scripts).Attributes += 'Hidden'
@@ -88,7 +107,7 @@ New-Item -itemtype Junction -path "$Env:userprofile" -name "Library" -value "C:\
 New-Item -itemtype "directory" -path "C:\Programs\Bin"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Programs\Bin", "Machine")
 
-Write-Output "Setting up symbolic links and directories..."
+Write-Host "Setting up symbolic links and directories..."
 New-Item -itemtype Junction -path "C:\" -name "Tmp" -value "$Env:temp"
 
 New-Item -itemtype "directory" -path "C:\Program Files (x86)\Steam\steamapps\common"
@@ -96,13 +115,16 @@ New-Item -itemtype Junction -path "C:\Programs\" -name "SteamApps" -value "C:\Pr
 
 
 # Setup cmd
-Write-Output "Configurating CMD..."
+Write-Host "Configurating CMD..." -ForegroundColor Blue
+
 Expand-Archive "$PSScriptRoot\..\WindowsBatchRC\batchrc.zip" -DestinationPath "$Env:userprofile"
-reg import "$PSScriptRoot\..\WindowsBatchRC\add_batchrc.reg"
+reg import "$PSScriptRoot\..\WindowsBatchRC\add_batchrc.reg" >$null 2>&1
+
+Write-Host "Configuration of CMD complete..." -ForegroundColor Green
 
 
 # Setup powershell profile
-Write-Output "Configurating Powershell..."
+Write-Host "Configurating Powershell..." -ForegroundColor Blue
 
 New-Item -itemtype "directory" -path "$Env:userprofile\Documents\PowerShell\"
 (get-item $Env:userprofile\Documents\PowerShell).Attributes += 'Hidden'
@@ -110,62 +132,69 @@ New-Item -itemtype "directory" -path "$Env:userprofile\Documents\WindowsPowerShe
 (get-item $Env:userprofile\Documents\WindowsPowerShell).Attributes += 'Hidden'
 
 if ([System.IO.File]::Exists("$PSScriptRoot\..\Library\PowershellProfile\profile.ps1")) {
-    Write-Output "Profile restored..."
+    Write-Host "Profile restored..."
     Copy-Item "$PSScriptRoot\..\Library\PowershellProfile\profile.ps1" -Destination "$Env:userprofile\Documents\PowerShell\"
 } else {
-    Write-Output "Creating empty profile..."
+    Write-Host "Creating empty profile..."
     New-Item -itemtype "file" -path "$Env:userprofile\Documents\PowerShell\profile.ps1"
 }
 New-Item -itemtype SymbolicLink -path "$Env:userprofile" -name ".psrc" -value "$Env:userprofile\Documents\PowerShell\profile.ps1"
 New-Item -itemtype SymbolicLink -path "$Env:userprofile\Documents\WindowsPowerShell" -name "profile.ps1" -value "$Env:userprofile\Documents\PowerShell\profile.ps1"
 (get-item $Env:userprofile\.psrc).Attributes += 'Hidden'
 
+Write-Host "Configuration of PowerShell complete..." -ForegroundColor Green
+
+
 
 # Creating quick links for terminal
-Write-Output "Setting up shims..."
+Write-Host "Setting up shims..." -ForegroundColor Blue
 New-Item -itemtype "directory" -path "C:\ProgramData\Chocolatey\shims"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\ProgramData\Chocolatey\shims", "Machine")
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\choco.exe" -p="C:\ProgramData\Chocolatey\choco.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\choco" -p="C:\ProgramData\Chocolatey\choco.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\choco.exe" -p="C:\ProgramData\Chocolatey\choco.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\choco" -p="C:\ProgramData\Chocolatey\choco.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\chrome.exe" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\chrome" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\chrome.exe" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\chrome" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\google-chrome.exe" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\google-chrome" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\google-chrome.exe" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\google-chrome" -p="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\steam.exe" -p="C:\Program Files (x86)\Steam\Steam.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\steam" -p="C:\Program Files (x86)\Steam\Steam.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\steam.exe" -p="C:\Program Files (x86)\Steam\Steam.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\steam" -p="C:\Program Files (x86)\Steam\Steam.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\code.exe" -p="C:\Program Files\Microsoft VS Code\Code.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\code" -p="C:\Program Files\Microsoft VS Code\Code.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\code.exe" -p="C:\Program Files\Microsoft VS Code\Code.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\code" -p="C:\Program Files\Microsoft VS Code\Code.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\spotify.exe" -p="$Env:userprofile\AppData\Roaming\Spotify\Spotify.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\spotify" -p="$Env:userprofile\AppData\Roaming\Spotify\Spotify.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\spotify.exe" -p="$Env:userprofile\AppData\Roaming\Spotify\Spotify.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\spotify" -p="$Env:userprofile\AppData\Roaming\Spotify\Spotify.exe" >$null 2>&1
 
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\TeamViewer.exe" -p="C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
-C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\TeamViewer" -p="C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\TeamViewer.exe" -p="C:\Program Files (x86)\TeamViewer\TeamViewer.exe" >$null 2>&1
+C:\ProgramData\Chocolatey\tools\shimgen.exe -o="C:\ProgramData\Chocolatey\shims\TeamViewer" -p="C:\Program Files (x86)\TeamViewer\TeamViewer.exe" >$null 2>&1
 
-Write-Output "Adjusting the context menu..."
-reg import "$PSScriptRoot\..\VSCode\Elevation_Add.reg"
 
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\!cleanUnwantedCreateNewFile.reg"
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewCppFile.reg"
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewHppFile.reg"
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewMdFile.reg"
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewPythonFile.reg"
-reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewSqfFile.reg"
+
+Write-Host "Adjusting the context menu..." -ForegroundColor Blue
+reg import "$PSScriptRoot\..\VSCode\Elevation_Add.reg" >$null 2>&1
+
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\!cleanUnwantedCreateNewFile.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewCppFile.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewHppFile.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewMdFile.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewPythonFile.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\CustomNewFileRegFiles\addCreateNewSqfFile.reg" >$null 2>&1
 
 # Terminals
-reg import "$PSScriptRoot\..\WindowsTerminal\add_windowsTerminal.reg"
-reg import "$PSScriptRoot\..\ContextMenuCMDnPowershell\CMDOnShellHack.reg"
-reg import "$PSScriptRoot\..\Powershell6\Add_Powershell6_context.reg"
+reg import "$PSScriptRoot\..\WindowsTerminal\add_windowsTerminal.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\ContextMenuCMDnPowershell\CMDOnShellHack.reg" >$null 2>&1
+reg import "$PSScriptRoot\..\Powershell6\Add_Powershell6_context.reg" >$null 2>&1
 
 # Change windows time
-reg import "$PSScriptRoot\..\WindowsUTCTime\Make Windows Use UTC Time.reg"
+reg import "$PSScriptRoot\..\WindowsUTCTime\Make Windows Use UTC Time.reg" >$null 2>&1
 
 # Remove VLC
-reg import "$PSScriptRoot\..\ContextMenuCleaners\remove_VLC.reg"
+reg import "$PSScriptRoot\..\ContextMenuCleaners\remove_VLC.reg" >$null 2>&1
+
+Write-Host "Context menu adjustment completed..." -ForegroundColor green
 
 Write-Host "Script completed." -ForegroundColor green
