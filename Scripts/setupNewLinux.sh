749de69cab159dd98a55febe4d047f3e0f9b5145
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-10-22 22:22:03 +0200

    Updated installer script

--- Scripts/setupNewLinux.sh
+++ Scripts/setupNewLinux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-IS_WSL=/mnt/c/WINDOWS/system32/wsl.exe
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-######################################################
+# Update apt dist
 sudo apt update -y
 sudo apt full-upgrade -y
 
@@ -12,11 +12,11 @@ sudo apt install ninja-build python3 python3-pip -y
 
 sudo apt install steam-installer -y
 
-######################################################
-if [ ! -x "$IS_WSL" ]; then
+# Setup and install snap
+if ! grep -iq 'microsoft' /proc/version &> /dev/null; then
     sudo apt install snapd -y
 
-    sudo snap install ripgrep
+    sudo snap install ripgrep --classic
 
     sudo snap install code --classic
 
@@ -24,107 +24,22 @@ if [ ! -x "$IS_WSL" ]; then
 
     sudo snap install discord
     sudo snap install slack --classic
-fi
-######################################################
 
-if [ -x "$IS_WSL" ]; then
-echo Adding a path mover to .bashrc...
-echo """
-# Set yout path in home if your in the system32 directory
-if [ \"\${PWD}\" == \"/mnt/c/Windows/system32\" ]; then
-    cd
+    sudo snap install scrcpy
+    sudo snap install vlc
 fi
-""" >> ~/.bashrc
+
+# Setup WSL if exist
+if grep -iq 'microsoft' /proc/version &> /dev/null; then
     python3 setupNewWSL.py
 fi
 
-######################################################
-echo Adding check for PATH in .bashrc...
-echo """
+# Setup bashrc and home
+cp "$SCRIPTPATH/../Library/bashrc/.bashrc" ~/.bashrc
+cp "$SCRIPTPATH/../Library/bashrc/.bash_path" ~/.bash_path
+cp "$SCRIPTPATH/../Library/bashrc/.bash_aliases" ~/.bash_aliases
 
-# PATH definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_path, instead of adding them here directly.
-if [ -f ~/.bash_path ]; then
-    . ~/.bash_path
-fi
-""" >> ~/.bashrc
-
-
-######################################################
-echo Setting up .scripts... 
+# Setup some scripts
 mkdir ~/.scripts
-echo """#!/usr/bin/env bash 
-
-set -e >> ~/.scripts/adb-push
-
-adb push $1 /sdcard/Download/$2
-echo $1: have been pushed to ./sdcard/Download/$2
-
-chmod +x ~/.scripts
-""" >> ~/.scripts/adb-push
-
-echo Setting up path... 
-echo """
-export PATH=\$PATH:~/.scripts
-
-# Android
-export ANDROID_HOME=$HOME/android-sdk
-export PATH=\$PATH:$ANDROID_HOME/tools/bin
-export PATH=\$PATH:$ANDROID_HOME/platform-tools
-""" >> ~/.bash_path
-
-
-######################################################
-echo Setting up .bash_aliases... 
-echo """ 
-alias dir='ls'
-
-alias sys-update='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y; sudo snap refresh; echo All updates are completed.'
-
-# Python
-alias pip='pip3'
-alias py='python3'
-alias python='python3'
-
-# Sudo
-alias inperium='sudo'
-
-# Program
-alias vim='nvim'
-
-alias ncspot='~/Library/ncspot/target/release/ncspot'
-alias spotify='ncspot'
-alias spotify-doc='google-chrome --app=https://github.com/hrkfdn/ncspot/blob/develop/README.md'
-
-alias chrome='google-chrome'
-alias screen='echo Running: scrcpy; scrcpy'
-alias youtube='chrome --app=https://youtube.com/'
-
-# ADB shorts
-alias adb-reverse='adb reverse tcp:8081 tcp:8081'
-alias adb-r='adb-reverse'
-alias adb-p='adb-push'
-
-alias rn-android-uninstall='adb-u; rn-a'
-alias rn-au='rn-android-uninstall'
-
-# Git shortcuts
-alias g-r='git rebase'
-alias g-f='git fetch --all --prune'
-
-alias g-fr='git fetch --all --prune; git rebase'
-alias g-rf='g-fr'
-
-alias g-s='git status'
-alias g-c='git checkout'
-alias g-u='git rebase master'
-
-alias g-p='git stash'
-alias g-pp='git stash pop'
-
-alias g-frp='g-p; g-rf'
-alias g-rfp='g-frp'
-alias g-frpp='g-frp; g-pp' 
-alias g-rfpp='g-rfpp'
-""" >> ~/.bash_aliases
+cp "$SCRIPTPATH/../Tools/adb-key" ~/.scripts/adb-key
+cp "$SCRIPTPATH/../Tools/adb-push" ~/.scripts/adb-push
