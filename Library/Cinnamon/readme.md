11d5ba19200137e17bffea3f9939a1f7ec15cb26
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-10-23 17:41:34 +0200

    updated and added backup

---
+++ Library/Cinnamon/readme.md
@@ -0,0 +1,7 @@
+Backup of the Cinnamon desktop enviroment 
+
+Saved via: `dconf dump /org/cinnamon/ > cinnamon_dconf`
+
+To restore run: `dconf load /org/cinnamon/ < cinnamon_dconf`
+
+The home contain zip contain home backups of cinnamon
--- Scripts/setupNewLinux.sh
+++ Scripts/setupNewLinux.sh
@@ -7,8 +7,10 @@ sudo apt update -y
 sudo apt full-upgrade -y
 
 sudo apt install git -y
-sudo apt install neovim android-sdk dos2unix -y
-sudo apt install ninja-build python3 python3-pip -y
+sudo apt install neovim android-sdk dos2unix jq -y
+
+sudo apt install cargo ninja-build -p
+sudo apt install python3 python3-pip -y
 
 sudo apt install steam-installer -y
 
@@ -20,6 +22,8 @@ if ! grep -iq 'microsoft' /proc/version &> /dev/null; then
 
     sudo snap install code --classic
 
+    sudo snap install powershell --classic
+
     sudo snap install spotify
 
     sudo snap install discord
@@ -28,6 +32,11 @@ if ! grep -iq 'microsoft' /proc/version &> /dev/null; then
     sudo snap install scrcpy
     sudo snap install vlc
 fi
+
+# Installing other files
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+rm ripgrep_11.0.2_amd64.deb
 
 # Setup WSL if exist
 if grep -iq 'microsoft' /proc/version &> /dev/null; then
