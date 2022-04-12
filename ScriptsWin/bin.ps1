8cc1567e81742e9ab49343a53f7f9a2e0748cc32
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-04-12 15:04:09 +0200

    better

--- ScriptsWin/bin.ps1
+++ ScriptsWin/bin.ps1
@@ -1,5 +1,13 @@
-$target    = $args[0]
-$name      = $args[1]
+$name       = $args[0]
+$altName    = $args[1]
+
+$target = which $name
+
+if (!$altName) {
+    $name = $altName
+}
 
 shimgen -p "$target" -o "C:\bin\$name" >$null 2>&1
 shimgen -p "$target" -o "C:\bin\$name.exe" >$null 2>&1
+
+write-host "'$target' have been added to bin"
