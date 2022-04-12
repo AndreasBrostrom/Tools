a5ab92079cd1e25143a73b0764927c64b2c57c67
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-04-12 15:05:32 +0200

    text update

--- ScriptsWin/bin.ps1
+++ ScriptsWin/bin.ps1
@@ -10,4 +10,4 @@ if (!$altName) {
 shimgen -p "$target" -o "C:\bin\$name" >$null 2>&1
 shimgen -p "$target" -o "C:\bin\$name.exe" >$null 2>&1
 
-write-host "'$target' have been added to bin"
+write-host "'$target' have been added to windows bin."
