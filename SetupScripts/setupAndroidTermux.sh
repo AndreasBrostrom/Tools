f08e54b34e72584a9de40d06c9e69b68c9fd405d
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-05-04 13:27:25 +0200

    better logging

--- SetupScripts/setupAndroidTermux.sh
+++ SetupScripts/setupAndroidTermux.sh
@@ -9,45 +9,51 @@ pkgInstall=(
 
 SCRIPTPATH="$( cd "$(dirname "$0")"; pwd -P )"
 
-echo -e "\e[1;34mPreforming full upgrade for all packages stand by...\e[0m"
+echo -e "\033[1mPreforming full upgrade for all packages stand by...\033[0m"
 pkg upgrade -y
 
 # Setup and install snap
-echo -e "\e[1;34mInstalling pkg packages...\e[0m"
+echo -e "\033[1mInstalling pkg packages\033[0m"
 for app in ${pkgInstall[@]}; do
-    echo "Installing $app and requirements..."
-    pkg install -y $app
+  echo -e " \033[2mInstalling $app and requirements...\033[0m"
+  pkg install -y $app
 done
 
-echo -e "\e[1;34mPreforming final checks and cleaning...\e[0m"
+echo -e "\033[1mPreforming final checks and cleaning...\033[0m"
 pkg upgrade -y
 
-echo -e "\e[1;34mSetting up home...\e[0m"
+echo -e "\033[1mSetting up home directory\033[0m"
 cd $HOME
 
 # Setting up home
+echo -e " \033[2mCreating folders\033[0m"
 [ ! -d "$HOME/Repositories" ] && mkdir -p $HOME/Repositories
 [ ! -d "$HOME/.bin" ]         && mkdir -p $HOME/.bin
+[ ! -d "$HOME/.ssh" ]         && mkdir -p $HOME/.ssh
 
-[ ! -d "$HOME/storage" ]       && ln -s /storage/emulated/0 storage
+echo -e " \033[2mLinking sdcard paths\033[0m"
+[ ! -d "$HOME/storage" ]      && ln -s /storage/emulated/0 storage
 [ ! -d "$HOME/Documents" ]    && ln -s /storage/emulated/0/Documents Documents
 [ ! -d "$HOME/Downloads" ]    && ln -s /storage/emulated/0/Download Downloads
 [ ! -d "$HOME/Pictures" ]     && ln -s /storage/emulated/0/Pictures Pictures
 [ ! -d "$HOME/Music" ]        && ln -s /storage/emulated/0/Music Music
 
-if [ ! -d "$HOME/Repositories/Tools" ]; then
-  cd $HOME/Repositories
-  git clone https://github.com/AndreasBrostrom/Tools.git
+# Setting up ssh permissions
+if [ -d "$HOME/.ssh" ]; then
+  echo -e "\033[1mFixing permission for ssh\033[0m"
+  chmod 700 $HOME/.ssh
+  chmod 600 $HOME/.ssh/config
+  chmod 600 $HOME/.ssh/authorized_keys
+  chmod 600 $HOME/.ssh/*id_rsa
+  chmod 644 $HOME/.ssh/*.pub
 fi
-sed -n 's#https://github.com/AndreasBrostrom/Tools.git#git@github.com:AndreasBrostrom/Tools.git#g' $HOME/Repositories/Tools/.git/config
-cd $HOME/Repositories/Tools/ScriptsLinux/
-cp * $HOME/.bin
 
+# clone and install tools and dotfiles
+echo -e "\033[1mSeting up dotfiles and tools\033[0m"
 if [ ! -d "$HOME/Repositories/dotfiles" ]; then
   cd $HOME/Repositories
-  git clone https://github.com/AndreasBrostrom/dotfiles.git
+  git clone git@github.com:AndreasBrostrom/Tools.git
 fi
-sed -n 's#https://github.com/AndreasBrostrom/dotfiles.git#git@github.com:AndreasBrostrom/dotfiles.git#g' $HOME/Repositories/dotfiles/.git/config
 cd $HOME/Repositories/dotfiles
 chmod +x install
 ./install
