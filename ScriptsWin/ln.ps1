e769b0bbf43c9ecad1490e9f7ae3ad4d33d94585
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-05-24 12:18:46 +0200

    removed shim and added path handling

--- ScriptsWin/ln.ps1
+++ ScriptsWin/ln.ps1
@@ -5,11 +5,11 @@ $target    = $args[1]
 $name      = $args[2]
 $option    = ""
 
+$targetFullPath = (Get-ChildItem $target).fullname
+
 if ( $operator -eq "-s" ) { $option = "SymbolicLink" }
 if ( $operator -eq "-j" ) { $option = "Junction" }
 if ( $operator -eq "-h" ) { $option = "HardLink" }
-if ( $operator -eq "--shim") { $option = "shim" }
-if ( $operator -eq "-m" ) { $option = "shim" }
 
 if ( !$args ) { "ln: missing file operand`nTry 'ln --help' for more information."; exit }
 
@@ -18,7 +18,6 @@ if ($operator -eq "--help" ) {
     "  -s            create a symbolic link"
     "  -j            create a junction"
     "  -h            create a hard link"
-    "  -m  --shim    create a shim if non exist else it gets copied"
     exit
 }
 
@@ -27,19 +26,8 @@ if ( !$args[1] ) { Write-Host "Missing argument: target" -ForegroundColor Red; e
 if ( !$args[2] ) { Write-Host "Missing argument: link" -ForegroundColor Red; exit }
 if ( !$args[0] ) { Write-Host "Missing options" -ForegroundColor Red; exit }
 
-
-if ( $option -ne "shim" ) {
-    if ( [System.IO.File]::Exists($target) -or [System.IO.Directory]::Exists($target) ) { 
-        New-Item -ItemType $option -Value $target -Path $name >$null 2>&1
-    } else {
-        Write-Host "Target '$target' does not exist..." -ForegroundColor Red
-    }
+if ( [System.IO.File]::Exists($target) -or [System.IO.Directory]::Exists($target) ) { 
+    New-Item -ItemType $option -Value $targetFullPath -Path $name >$null 2>&1
 } else {
-    if ( "$target" -like "*shim*" ) {
-        Write-Host "'$target' is already a shim copying instead..." -ForegroundColor Yellow
-        Copy-Item "$target" -Destination "$PWD" >$null 2>&1
-    } else {
-        Write-Host "Creating shim" -ForegroundColor White
-        shimgen -p "$target" -o "$PWD\$name" >$null 2>&1
-    }
+    Write-Host "Target '$target' does not exist..." -ForegroundColor Red
 }
