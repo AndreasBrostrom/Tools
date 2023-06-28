85d2386f8e64607c28ca2ce8a0570b45872ced0d
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-28 12:19:09 +0200

    update

--- installScripts.ps1
+++ installScripts.ps1
@@ -1,11 +1,15 @@
-#Copy-Item -Path "ScriptsWin" -Destination C:\Programs\Bin
-#
-#New-Item -ItemType SymbolicLink -Path "Link" -Target "Target"
-#
-#Get-ChildItem
-#
-Write-host "Copying scripts to C:/bin/."
+#!/bin/pwsh
+
+if ( -Not $IsWindows ) {
+    Write-host "This script does not work on non windows system"
+    exit 1
+}
+$desitnation = if (args[0] -eq "-user") { "$($env:USERPROFILE)/.bin" } else { "C:/bin"}
+    
+
+Write-host "Copying scripts to $desitnation/."
 ForEach ($filePath in Get-ChildItem "ScriptsWin") {
-    #Write-host "$($filePath.name) -> C:/bin/$($filePath.name)" 
-    New-Item -ItemType SymbolicLink -Target "$filePath" -Path "C:/bin/$($filePath.name)" -Force
+    Write-host "$($filePath.name) -> "$desitnation/$($filePath.name)" 
+    $path = "$($desitnation)/$($filePath.name)"
+    #New-Item -ItemType SymbolicLink -Target "$filePath" -Path $path -Force
 }
