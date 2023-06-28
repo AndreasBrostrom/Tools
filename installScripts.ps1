e1442bbad7a202123d85eb733203e40e0f092ee4
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-28 12:23:07 +0200

    update

--- installScripts.ps1
+++ installScripts.ps1
@@ -4,13 +4,13 @@ if ( -Not $IsWindows ) {
     Write-host "This script does not work on non windows system"
     exit 1
 }
-$desitnation = if (args[0] -eq "-user") { "$($env:USERPROFILE)/.bin" } else { "C:/bin"}
+$desitnation = if ($args[0] -eq "-user") { "$($env:USERPROFILE)\.bin" } else { "C:\bin"}
     
 
-Write-host "Copying scripts to $desitnation/."
+Write-host "Copying scripts to $desitnation\."
 
 ForEach ($filePath in Get-ChildItem "ScriptsWin") {
-    Write-host "$($filePath.name) -> $desitnation/$($filePath.name)" 
-    $path = "$($desitnation)/$($filePath.name)"
-    #New-Item -ItemType SymbolicLink -Target "$filePath" -Path $path -Force
+    #Write-host "$($filePath.name) -> $desitnation\$($filePath.name)" 
+    $path = "$($desitnation)\$($filePath.name)"
+    New-Item -ItemType SymbolicLink -Target "$filePath" -Path $path -Force
 }
