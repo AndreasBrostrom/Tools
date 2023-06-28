9f20e13de5d003446c5a8db60d840169febbc09e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-28 11:54:38 +0200

    update

--- installScripts.ps1
+++ installScripts.ps1
@@ -4,9 +4,8 @@
 #
 #Get-ChildItem
 #
-Write-host Copying scripts to /bin
-
+Write-host "Copying scripts to C:/bin/."
 ForEach ($filePath in Get-ChildItem "ScriptsWin") {
-    Write-host "$filePath ==> C:/bin/$filePath" 
-    #New-Item -ItemType SymbolicLink -Path $filePath -Target "C:/bin/$filePath.name" -Force
+    #Write-host "$($filePath.name) -> C:/bin/$($filePath.name)" 
+    New-Item -ItemType SymbolicLink -Path "$filePath" -Target "C:/bin/$($filePath.name)" -Force
 }
