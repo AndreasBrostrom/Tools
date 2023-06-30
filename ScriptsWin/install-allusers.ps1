be8df30badbe6be31a08ee2488483a51ee4c853a
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-06-30 16:59:05 +0200

    Update

---
+++ ScriptsWin/install-allusers.ps1
@@ -0,0 +1,28 @@
+if ( -not [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    Write-Host "$([io.path]::GetFileNameWithoutExtension("$($MyInvocation.MyCommand.Name)")) is not running as Administrator. Start PowerShell by using the Run as Administrator option" -ForegroundColor Red -NoNewline
+
+    # check if have sudo programs installed
+    $sudoScripts =  "$env:USERPROFILE\scoop\shims\sudo",
+                    "$env:USERPROFILE\scoop\shims\sudo.ps1",
+                    "$env:PROGRAMDATA\scoop\shims\sudo",
+                    "$env:PROGRAMDATA\scoop\shims\sudo.ps1",
+                    "$env:PROGRAMDATA\chocolatey\bin\Sudo.exe",
+                    "$env:USERPROFILE\.bin\sudo.ps1"
+
+    foreach ($sudoScript in $sudoScripts) { if ( [System.IO.File]::Exists("$sudoScript") ) { [bool] $hasSudo = 1; break } }
+    if ($hasSudo) { Write-Host " or run with sudo" -ForegroundColor Red -NoNewline }
+    
+    Write-Host ", and then running the script again." -ForegroundColor Red
+    exit 1
+}
+
+$FilePath = Get-Item -LiteralPath $args[0]
+
+if ( -Not $IsWindows ) {
+    Write-host "This script does not work on none-windows system"
+    exit 1
+}
+$Dest = "C:\bin"
+    
+$Path = "$($dest)\$($FilePath.name)"
+New-Item -ItemType SymbolicLink -Target "$FilePath" -Path $Path -Force
--- installScripts.ps1
+++ installScripts.ps1
@@ -1,16 +1,33 @@
-#!/bin/pwsh
-
 if ( -Not $IsWindows ) {
-    Write-host "This script does not work on non windows system"
+    Write-host "This script does not work on none-windows system" -ForegroundColor Red
     exit 1
 }
-$desitnation = "$($env:USERPROFILE)\.bin"
-    
 
-Write-host "Copying scripts to $desitnation\."
+if ( -not [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    Write-Host "$([io.path]::GetFileNameWithoutExtension("$($MyInvocation.MyCommand.Name)")) is not running as Administrator. Start PowerShell by using the Run as Administrator option" -ForegroundColor Red -NoNewline
+
+    
+    # check if have sudo programs installed
+    $sudoScripts =  "$env:USERPROFILE\scoop\shims\sudo",
+                    "$env:USERPROFILE\scoop\shims\sudo.ps1",
+                    "$env:PROGRAMDATA\scoop\shims\sudo",
+                    "$env:PROGRAMDATA\scoop\shims\sudo.ps1",
+                    "$env:PROGRAMDATA\chocolatey\bin\Sudo.exe",
+                    "$env:USERPROFILE\.bin\sudo.ps1"
+
+    foreach ($sudoScript in $sudoScripts) { if ( [System.IO.File]::Exists("$sudoScript") ) { [bool] $hasSudo = 1; break } }
+    if ($hasSudo) { Write-Host " or run with sudo" -ForegroundColor Red -NoNewline }
+    
+    Write-Host ", and then running the script again." -ForegroundColor Red
+    exit 1
+}
+
+$dest = "$($env:USERPROFILE)\.bin"
+
+Write-host "Copying scripts to $dest\."
 
 ForEach ($filePath in Get-ChildItem "ScriptsWin") {
-    #Write-host "$($filePath.name) -> $desitnation\$($filePath.name)" 
-    $path = "$($desitnation)\$($filePath.name)"
+    #Write-host "$($filePath.name) -> $dest\$($filePath.name)" 
+    $path = "$($dest)\$($filePath.name)"
     New-Item -ItemType SymbolicLink -Target "$filePath" -Path $path -Force
 }
