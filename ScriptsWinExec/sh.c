7dc95ab5a37c3fcb80395fdc4b7409a271d97ad6
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-09-14 15:46:13 +0200

    added better code

--- ScriptsWinExec/sh.c
+++ ScriptsWinExec/sh.c
@@ -2,25 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+static const char command[] = "C:/Windows/system32/wsl.exe --distribution Arch --exec sh ";
+
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    int cmdlen = 62;
-    for (int i = 1; i < argc; i++) {
-      cmdlen += strlen(argv[0])+1;
-    };
+  int cmdlen = sizeof(command)+1; // Length of string with null
+  for (int i = 1; i < argc; i++) {
+    cmdlen += strlen(argv[i])+3;
+  };
 
-    char *cmd = malloc(sizeof(char)*cmdlen);
-    strcpy(cmd, "C:/Windows/system32/wsl.exe --distribution Arch --exec sh "); // add ls to start of cmd
+  char *cmd = malloc(cmdlen);
+  strcpy(cmd, command); // add ls to start of cmd
 
-    int charcount = 60;
-    for (int i = 1; i < argc; i++) {
-      strcpy(cmd+charcount, argv[i]);
-      charcount += strlen(argv[i])+1;
-      cmd[charcount-1] = ' ';
-    };
-    system(cmd);
-  } else {
-    system("C:/Windows/system32/wsl.exe --distribution Arch --exec sh");
-  }
-  return 0;
+  for (int i = 1; i < argc; i++) {
+    strcat(cmd, "'"); // string (" ") and null byte at end due to " "
+    strcat(cmd, argv[i]);
+    strcat(cmd, "' "); // string (" ") and null byte at end due to " "
+  };
+  
+  return system(cmd) >> 8;
 }
