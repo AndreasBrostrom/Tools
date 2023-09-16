9d607da9c600ac3037c72a9c18d69cc7cb195a62
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-09-16 09:34:21 +0200

    Update

---
+++ ScriptsWinExec/build.cmd
@@ -0,0 +1,11 @@
+gcc arma.c -o arma.exe
+gcc arma.c -o arma3.exe
+gcc discord.c -o discord.exe
+gcc hoi4.c -o hoi4.exe
+gcc stellaris.c -o stellaris.exe
+gcc warhammer.c -o warhammer.exe
+gcc warno.c -o warno.exe
+
+gcc bash.c -o bash.exe
+gcc fish.c -o fish.exe
+gcc sh.c -o sh.exe
