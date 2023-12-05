2b31d418fb55d24e6c1f65ce2363555e9c382063
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-12-05 12:21:32 +0100

    updated

--- SetupScripts/setupArchI3.sh
+++ SetupScripts/setupArchI3.sh
@@ -4,9 +4,6 @@ sudo -v
 pacmanInstall=(
   git base-devel
   neovim
-  
-  xorg-server
-  pulseaudio
 )
 
 paruInstall=(
@@ -24,6 +21,7 @@ paruInstall=(
   
   terminator
   fish starship
+  thunar
   
   gparted gnome-disk-utility
   
@@ -66,21 +64,18 @@ if [ ${#pacmanInstall[@]} -eq 0 ]; then
 fi
 
 # Install pary
-cd /opt
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd
+#sudo pacman -S --needed base-devel
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si
+#cd
 
-# Install yay packages
-if [ ${#paruInstall[@]} -eq 0 ]; then
-  echo -e "\e[1;34mInstalling yay packages...\e[0m"
+# Install paru packages
+  echo -e "\e[1;34mInstalling paru packages...\e[0m"
   for app in ${paruInstall[@]}; do
       echo "Installing $app and requirements..."
       yes "" | paru -Sy $app
   done
-fi
 
 echo -e "\e[1;34mPreforming final checks and cleaning...\e[0m"
 yes "" | paru -Syyu
@@ -115,11 +110,11 @@ chmod 600 $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/*id_rsa
 chmod 644 $HOME/.ssh/*.pub
 
-cd $HOME/Repositories
-git clone git@github.com:AndreasBrostrom/dotfiles.git
-cd $HOME/Repositories/dotfiles
-chmod +x install
-./install
+#cd $HOME/Repositories
+#git clone git@github.com:AndreasBrostrom/dotfiles.git
+#cd $HOME/Repositories/dotfiles
+#chmod +x install
+#./install
 
 sudo --reset-timestamp
 echo -e "done"
