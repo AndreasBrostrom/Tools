ad3b157b8e71cc80494b92bc8b97293828a772fa
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-04-29 19:23:03 +0200

    Minor fixes

--- ScriptsWin/ip.ps1
+++ ScriptsWin/ip.ps1
@@ -9,7 +9,6 @@ if ( $operator -eq "a" ) { $option = "address" }
 if ( $operator -eq "ad" ) { $option = "address" }
 if ( $operator -eq "add" ) { $option = "address" }
 if ( $operator -eq "addr" ) { $option = "address" }
-if ( $operator -eq "addr" ) { $option = "address" }
 if ( $operator -eq "addre" ) { $option = "address" }
 if ( $operator -eq "addres" ) { $option = "address" }
 if ( $operator -eq "address" ) { $option = "address" }
@@ -19,11 +18,12 @@ if ( ($operator -eq "help") -or !($args) -or !($args[0]) ) {
     "Usage: ip OBJECT { COMMAND | help }"
     "where  OBJECT := { address }"
     ""
-    exit 1
+    exit 0
 }
 
 if ( $option -notin "address" ) { Write-Host "'$operator' invalid option" -ForegroundColor Red; exit }
 
 if ( $option -eq "address" ) { 
-    Get-NetIPConfiguration -All; exit 0
+    Get-NetIPConfiguration -All
+    exit 0
 }
